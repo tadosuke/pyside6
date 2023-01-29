270f377ce673c0fc6b12e9ef4bf22a136a9f48b1
tadosuke <tadosuke@gmail.com>
2023-01-29 17:46:01 +0900

    QStringListModel のサンプル

---
+++ stringlist_model.py
@@ -0,0 +1,77 @@
+"""QStringListModel のサンプル."""
+
+from PySide6.QtCore import QStringListModel, QModelIndex
+from PySide6.QtWidgets import QApplication, QComboBox, QWidget, QMainWindow, QStatusBar, QListView, QTableView
+
+
+def _on_data_changed(top_left: QModelIndex, bottom_right: QModelIndex) -> None:
+    """データが変更された."""
+
+    print(f'dataChanged(topLeft={top_left}, bottom_right={bottom_right})')
+
+
+def _create_combobox() -> QComboBox:
+    """QComboBox を生成する."""
+
+    model = QStringListModel()
+    model.dataChanged.connect(_on_data_changed)
+    model.setStringList(['item1', 'item2', 'item3'])
+    print(f'stringsList = {model.stringList()}')
+
+    widget = QComboBox()
+    widget.setModel(model)
+
+    return widget
+
+
+def _create_listview() -> QListView:
+    """QListView を生成する."""
+
+    model = QStringListModel()
+    model.dataChanged.connect(_on_data_changed)
+    model.setStringList(['item1', 'item2', 'item3'])
+
+    # 行の挿入もできるが、item を直接指定できないため使いにくい
+    model.insertRow(2)
+    model.setData(model.index(2), 'item4')  # dataChanged が呼ばれる
+
+    widget = QListView()
+    widget.setModel(model)
+
+    return widget
+
+
+def _create_tableview() -> QTableView:
+    """QTableView を生成する.
+
+    StringList は1次元データなので、2次元のビューには不向き
+    """
+
+    model = QStringListModel()
+    model.dataChanged.connect(_on_data_changed)
+    model.setStringList(['item1', 'item2', 'item3'])
+
+    widget = QTableView()
+    widget.setModel(model)
+
+    return widget
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self, parent: QWidget = None) -> None:
+        super().__init__(parent)
+
+        # widget = _create_combobox()
+        widget = _create_listview()
+        # widget = _create_tableview()
+
+        self.setCentralWidget(widget)
+        self.setStatusBar(QStatusBar())
+
+
+if __name__ == '__main__':
+    app = QApplication()
+    window = MainWindow()
+    window.show()
+    app.exec()
