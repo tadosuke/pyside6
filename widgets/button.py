744d5b42f27f039ab9e747a119a0d537c144bac0
tadosuke <tadosuke@gmail.com>
2023-01-31 23:05:51 +0900

    QPushButton のサンプル

--- widgets/button.py
+++ widgets/button.py
@@ -1,6 +1,8 @@
 """ QPushButton のサンプル."""
 
 from PySide6 import QtWidgets
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QMenu, QPushButton
 
 
 class MainWindow(QtWidgets.QWidget):
@@ -9,10 +11,50 @@ class MainWindow(QtWidgets.QWidget):
     def __init__(self) -> None:
         super().__init__()
 
-        button = QtWidgets.QPushButton("Button 1", self)
+        button = QPushButton("Button 1", self)
+
+        # テキストを設定する
+        # button.setText('Changed')
+
+        # シグナルへの接続
         button.clicked.connect(self._clicked_button)
         button.pressed.connect(self._pressed_button)
         button.released.connect(self._released_button)
+
+        # クリックしたことにする
+        # button.click()
+
+        # ボタンを押し続けている間、一定間隔でシグナルを発火させる
+        # button.setAutoRepeat(True)
+        # リピートするまでの時間（ミリ秒）
+        # button.setAutoRepeatDelay(1000)
+        # リピート間隔
+        # button.setAutoRepeatInterval(500)
+
+        # ウィジェット上で Enter キーを押したとき、このボタンを押したことにする
+        # button.setDefault(True)
+
+        # フラットスタイルにする
+        # button.setFlat(True)
+
+        # ボタンを押したときにメニューを出すようにする
+        self._add_menu(button)
+
+    def _add_menu(self, button: QPushButton) -> None:
+        """ボタンにメニューを追加する."""
+
+        menu = QMenu(parent=self)
+
+        action = QAction('menu', self)
+        action.triggered.connect(self._menu_selected)
+        menu.addAction(action)
+
+        button.setMenu(menu)
+
+    def _menu_selected(self) -> None:
+        """メニューが選択された."""
+
+        print('menu selected')
 
     def _clicked_button(self) -> None:
         """ボタンがクリックされた."""
@@ -28,6 +70,11 @@ class MainWindow(QtWidgets.QWidget):
         """ボタンが離された."""
 
         print("button1 released")
+
+    def _toggled_button(self) -> None:
+        """ボタンのトグル状態が切り替わった."""
+
+        print("button1 toggled")
 
 
 if __name__ == '__main__':
