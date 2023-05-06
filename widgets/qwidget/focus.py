10aa9d1ad2ad5acbc1caef74e4c827223fbfbea4
tadosuke <tadosuke@gmail.com>
2023-05-06 17:40:06 +0900

    ウィジェットのフォーカスを設定/取得するサンプル

---
+++ widgets/qwidget/focus.py
@@ -0,0 +1,25 @@
+"""ウィジェットのフォーカスを設定/取得する."""
+
+from PySide6 import QtWidgets
+
+app = QtWidgets.QApplication()
+
+# 準備
+window = QtWidgets.QWidget()
+edit1 = QtWidgets.QLineEdit()
+edit2 = QtWidgets.QLineEdit()
+layout = QtWidgets.QVBoxLayout()
+layout.addWidget(edit1)
+layout.addWidget(edit2)
+window.setLayout(layout)
+window.show()
+
+# 設定
+#edit1.setFocus()
+edit2.setFocus()
+
+# 取得
+print(edit1.hasFocus())
+print(edit2.hasFocus())
+
+app.exec()
