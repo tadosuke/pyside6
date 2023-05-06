45dbb6af1b7eb90f1033a673a4bcb3022cc4895a
tadosuke <tadosuke@gmail.com>
2023-05-06 16:43:21 +0900

    誤って上書きしてしまったコードを戻した

--- widgets/qwidget/window_parent.py
+++ widgets/qwidget/window_parent.py
@@ -1,14 +1,16 @@
-"""ウィジェットの有効/無効を設定/取得する."""
+"""ウィジェットの親を設定/取得する."""
 
 from PySide6 import QtWidgets
 
 app = QtWidgets.QApplication()
-button = QtWidgets.QPushButton('無効')
 
 # 設定
-button.setEnabled(False)
-# 取得
-print(button.isEnabled())  # False
+window1 = QtWidgets.QWidget()
+window2 = QtWidgets.QPushButton('子ウィジェット', parent=window1)
+window1.show()
 
-button.show()
+# 取得
+print(window1.parent())  # None
+print(window2.parent())  # window1
+
 app.exec()
