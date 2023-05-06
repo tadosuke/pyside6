ba9f34b58265a4baf18a6ccb47c5831561970ef2
tadosuke <tadosuke@gmail.com>
2023-05-07 08:01:31 +0900

    スタイルシートの内容を修正

--- widgets/qwidget/stylesheet.py
+++ widgets/qwidget/stylesheet.py
@@ -3,10 +3,14 @@
 from PySide6 import QtWidgets
 
 app = QtWidgets.QApplication()
-window = QtWidgets.QWidget()
+window = QtWidgets.QLabel('テキスト')
 
 # 設定
-window.setStyleSheet("background-color: yellow;")
+window.setStyleSheet(
+    "background-color: yellow;"
+    "color: red;"
+    "margin: 10px;"
+)
 
 # 取得
 print(window.styleSheet())  # "background-color: yellow;"
