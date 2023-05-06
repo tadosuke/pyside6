50a4534dab539894bd26e6989b5f280762ca0206
tadosuke <tadosuke@gmail.com>
2023-05-06 16:40:26 +0900

    ウィジェットの有効/無効を設定/取得するサンプル

--- widgets/qwidget/window_parent.py
+++ widgets/qwidget/window_parent.py
@@ -1,16 +1,14 @@
-"""ウィジェットの親を設定/取得する."""
+"""ウィジェットの有効/無効を設定/取得する."""
 
 from PySide6 import QtWidgets
 
 app = QtWidgets.QApplication()
+button = QtWidgets.QPushButton('無効')
 
 # 設定
-window1 = QtWidgets.QWidget()
-window2 = QtWidgets.QPushButton('子ウィジェット', parent=window1)
-window1.show()
-
+button.setEnabled(False)
 # 取得
-print(window1.parent())  # None
-print(window2.parent())  # window1
+print(button.isEnabled())  # False
 
+button.show()
 app.exec()
