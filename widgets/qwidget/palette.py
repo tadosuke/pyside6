c84d851d82c5fe050bd9e4cd4b5c55609ef9eb26
tadosuke <tadosuke@gmail.com>
2023-05-13 09:11:49 +0900

    setAutoFillBackground の有無について説明を追加

--- widgets/qwidget/palette.py
+++ widgets/qwidget/palette.py
@@ -3,17 +3,29 @@
 from PySide6 import QtWidgets
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPalette
+from PySide6.QtWidgets import QLabel
 
 app = QtWidgets.QApplication()
 window = QtWidgets.QWidget()
+label = QLabel('テキスト', parent=window)
 
-# 設定
-palette = window.palette()
-palette.setColor(QPalette.ColorRole.Window, Qt.red)  # ウィンドウ色を赤に
-window.setPalette(palette)
+# パレットの設定
+window_palette = window.palette()
+window_palette.setColor(QPalette.ColorRole.Window, Qt.red)
+label_palette = label.palette()
+label_palette.setColor(QPalette.ColorRole.Window, Qt.blue)
+
+# 子ウィジェットに設定
+label.setPalette(label_palette)
+label.setAutoFillBackground(True)  # 背景色を塗りつぶすオプション
+
+# 親ウィンドウに設定
+window.setPalette(window_palette)
+# label.setAutoFillBackground(True)  # ウィンドウの場合は指定しなくても塗りつぶされる
 
 # 取得
-print(window.palette().color(QPalette.ColorRole.Window))
+print(window_palette.color(QPalette.ColorRole.Window))
+print(label_palette.color(QPalette.ColorRole.Window))
 
 window.show()
 app.exec()
