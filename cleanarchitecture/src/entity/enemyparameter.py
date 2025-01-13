7a1554e1746fad70b33b51088da8d32c8304d8ac
tadosuke <tadosuke@gmail.com>
2025-01-13 13:32:00 +0900

    valueobject を一旦廃止

--- cleanarchitecture/src/entity/enemyparameter.py
+++ cleanarchitecture/src/entity/enemyparameter.py
@@ -1,10 +1,8 @@
 """エネミーのパラメータを保持するモジュール."""
 
-from valueobjects import Hp
-
 
 class EnemyParameter:
 
     def __init__(self) -> None:
         self._name = ''
-        self._hp = Hp(0)
+        self._hp = 0
