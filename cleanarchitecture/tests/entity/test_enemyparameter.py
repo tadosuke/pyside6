b089bb1b9fbb12b55a31c712f4eabb1118d5728e
tadosuke <tadosuke@gmail.com>
2025-01-13 13:17:52 +0900

    enemyparameter を仮実装

---
+++ cleanarchitecture/tests/entity/test_enemyparameter.py
@@ -0,0 +1,16 @@
+"""entity.enemyparameter モジュールのテスト."""
+
+import unittest
+
+from entity.enemyparameter import EnemyParameter
+
+
+class TestEnemyParameter(unittest.TestCase):
+
+    def test_init(self):
+        """生成後の状態が正しいか？"""
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
